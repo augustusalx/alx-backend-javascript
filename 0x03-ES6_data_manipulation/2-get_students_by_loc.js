8bd9051b0c6fb44ebf207f8106820a2ac2aaf24e
Augustusalx <Augustusemmanuel91@gmail.com>
2023-07-31 16:19:55 -0700

    completed

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Will retrieve students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Augustus Emmanuel
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
